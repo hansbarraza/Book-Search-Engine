a63f053c6d2213048adccfce4908172961126527
Hans Barraza <brain@Hanss-MacBook-Pro.local>
2023-04-18 19:48:41 -0700

    modified scripts on package.json

--- server/server.js
+++ server/server.js
@@ -29,9 +29,9 @@ if (process.env.NODE_ENV === 'production') {
 //   res.sendFile(path.join(__dirname, '../client/'));
 // });
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../client/build/index.html"));
+// });
 
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
