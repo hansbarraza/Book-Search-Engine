c8e3c505f94f11d3535b8a4ac3fbdc4c3a3bcc99
Hans Barraza <brain@Hanss-MacBook-Pro.local>
2023-04-19 17:17:12 -0700

    modified scripts to deploy

--- server/server.js
+++ server/server.js
@@ -21,13 +21,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '../client/build')));
+// }
 
 // app.get('/', (req, res) => {
 //   res.sendFile(path.join(__dirname, '../client/'));
 // });
+
+app.use(express.static('public'));
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
