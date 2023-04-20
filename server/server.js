f9669cd25b6b9673d092717c2fdedaf0422e6a5e
Hans Barraza <brain@Hanss-MacBook-Pro.local>
2023-04-19 18:19:23 -0700

    added node version in package.json

--- server/server.js
+++ server/server.js
@@ -21,19 +21,19 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, '../client/build')));
-// }
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
 
 // app.get('/', (req, res) => {
 //   res.sendFile(path.join(__dirname, '../client/'));
 // });
 
-app.use(express.static('public'));
+// app.use(express.static('public'));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../client/build/index.html"));
+// });
 
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
